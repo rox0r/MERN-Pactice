7dbd74a4f628ac622299e8fe4901482363630e4f
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-05 15:17:03 +0530

    Create POST req to log input w/ Express

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 var app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.fname);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
