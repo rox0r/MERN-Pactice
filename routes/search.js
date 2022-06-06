f670c0eaf7dfe2071552582592c311dddf022cda
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-06 10:20:48 +0530

    Create search POST route

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 var router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/", (req, res) => {
   let dirPath = path.join(__dirname, "..", "views", "search.html");
   res.sendFile(dirPath);
+});
+
+router.post("/", (req, res) => {
+  res.redirect("/search/" + req.body.searchQuery);
 });
 
 module.exports = router;
