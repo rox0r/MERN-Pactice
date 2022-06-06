86a0d0b4f2d102d9d0d6bd14a790c39d77e79af3
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-06 11:41:08 +0530

    Add RegExp pattern with word length validation to search result GET route

--- routes/searchResult.js
+++ routes/searchResult.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 var router = express.Router();
 
-router.get("/:query", (req, res) => {
+router.get("/:query([a-zA-Z]{1,5})", (req, res) => {
   let dirPath = path.join(__dirname, "..", "views", "searchResult.html");
   res.sendFile(dirPath);
 });
