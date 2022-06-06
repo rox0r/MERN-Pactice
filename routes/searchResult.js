6eea765d217dfe160eec488bd34f27b1da22146d
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-06 12:59:37 +0530

    Add Middlware to search result routes file
    It will log the query parameter of search request.

--- routes/searchResult.js
+++ routes/searchResult.js
@@ -5,6 +5,14 @@ var router = express.Router();
 
 const err404 = require("./Errors/404");
 
+//Middleware: pre-check/process before sending to actual route
+//Order of these routes is very important.
+
+router.use("/:query([a-zA-Z]{1,5})", (req, res, next) => {
+  console.log("Middleware captured: " + req.params.query);
+  next(); // this runs the next route, i.e the code just below this.
+});
+
 router.get("/:query([a-zA-Z]{1,5})", (req, res) => {
   let dirPath = path.join(__dirname, "..", "views", "searchResult.html");
   res.sendFile(dirPath);
