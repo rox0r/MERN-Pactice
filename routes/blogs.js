8aa4b7efbac4cde8c8464eae56367becab86c864
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-08 13:50:54 +0530

    Render Blog collections via blogs route

--- routes/blogs.js
+++ routes/blogs.js
@@ -6,6 +6,7 @@ const path = require("path");
 const { dbConnect, dbDisconnect } = require("../config/db");
 const Blog = require("../models/Blog");
 const Author = require("../models/Author");
+const { log } = require("console");
 
 var router = express.Router();
 dbConnect("blogsDB");
@@ -21,22 +22,15 @@ const author = new Author({ name: "John", age: "28" });
 //author.save();
 //blog.save();
 
-var blogsArr = [
-  {
-    heading: "Lorem Ipsum simply",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-  },
-  {
-    heading: "Printing and typesetting",
-    content:
-      "Lorem Ipsum is dummy text of the printing and typesetting industry. ",
-  },
-];
-
 router.get("/", (req, res) => {
-  let dirPath = path.join(__dirname, "..", "views", "blogs.ejs");
-  res.render(dirPath, { blogs: blogsArr });
+  Blog.find({}, (err, blogsArr) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let dirPath = path.join(__dirname, "..", "views", "blogs.ejs");
+      res.render(dirPath, { blogs: blogsArr });
+    }
+  });
 });
 
 module.exports = router;
