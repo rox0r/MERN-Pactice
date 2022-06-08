b2fd3a9402e35ab39bb4d4f1c5ef2cd05abb8097
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-08 14:15:34 +0530

    Add validation to blogs schema

--- models/Blog.js
+++ models/Blog.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-  name: String,
-  heading: String,
-  content: String,
+  name: {
+    type: String,
+    required: [true, "Name is required."],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: [true, "Rating must be between 1-5."],
+  },
+  heading: {
+    type: String,
+    required: [true, "Heading is required."],
+  },
+  content: {
+    type: String,
+    required: [true, "Content is required."],
+  },
 });
 
 const Blog = new mongoose.model("Blog", blogSchema);
